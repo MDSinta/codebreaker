1afdd0ef72e4ffdab0ab9428ef0e419e0541ad79
MDSinta <mdtasinr2789@nycstudents.net>
2023-10-09 23:17:23 -0400

    99

--- main.js
+++ main.js
@@ -1,27 +1,5 @@
-const body = document.body.querySelector("button");
+const answer = document.getElementById("number");
 
-const r1c1 = document.body.q.querySelector("one");
-const r1c2 = document.body.q.querySelector("two");
-const r1c3 = document.body.q.querySelector("three");
-
-const r2c1 = document.body.w.querySelector("one");
-const r2c1 = document.body.w.querySelector("two");
-const r2c3 = document.body.w.querySelector("three");
-
-const r3c1 = document.body.e.querySelector("one");
-const r3c2 = document.body.e.querySelector("two");
-const r3c3 = document.body.e.querySelector("three");
-
-const r4c1 = document.body.r.querySelector("one");
-const r4c2 = document.body.r.querySelector("two");
-const r4c3 = document.body.r.querySelector("three");
-
-const r5c1 = document.body.t.querySelector("one");
-const r5c2 = document.body.t.querySelector("two");
-const r5c3 = document.body.t.querySelector("three");
-
-const r6c1 = document.body.y.querySelector("one");
-const r6c2 = document.body.y.querySelector("two");
-const r6c3 = document.body.y.querySelector("three");
-
-
+function display() {
+    console.log(document.getElementById("joe").textContent);
+}
