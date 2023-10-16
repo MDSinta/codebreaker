c6e5c7a7564c885ae95e3b64d99e01bc3aae0cd2
MDSinta <mdtasinr2789@nycstudents.net>
2023-10-16 15:05:48 -0400

    10000

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-const answer = document.getElementById("number");
+const answer = document.querySelector("input");
+count = 1;
+num = Math.floor(Math.random()*100);
 
+console.log(num);
 function display() {
-    console.log(document.getElementById("joe").textContent);
+    if (answer.value < 999) {
+        document.getElementById("guess"+count).innerHTML = answer.value;
+        if(num>answer.value) 
+        {
+            document.getElementById("guess"+count).parentElement.style.backgroundColor= 'red';
+        }
+        if(num<answer.value) 
+        {
+            document.getElementById("guess"+count).parentElement.style.backgroundColor= 'yellow';
+        }
+        if(num == answer.value) {
+            document.getElementById("guess"+count).parentElement.style.backgroundColor= 'green';
+            alert("you win");
+            location.reload();
+        }
+
+        count++;
+    }
+    else alert("enter a different number less than 1000")
 }
